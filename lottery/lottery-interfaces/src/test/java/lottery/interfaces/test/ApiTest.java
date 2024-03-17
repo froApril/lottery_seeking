52a690e9e54c98c200ec8d8f7a44074c466e8663
FroApril <188494506@qq.com>
2024-03-17 20:09:29 +1100

    freddie, feat: rpc components first version

---
+++ lottery/lottery-interfaces/src/test/java/lottery/interfaces/test/ApiTest.java
@@ -0,0 +1,46 @@
+package lottery.interfaces.test;
+
+import com.alibaba.fastjson.JSON;
+import lottery.infrastructure.dao.activity.IActivityDao;
+import lottery.infrastructure.po.activity.Activity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Resource
+    IActivityDao activityDao;
+
+    @Test
+    public void test_insert() {
+        Activity activity = new Activity();
+        activity.setActivityId(100002L);
+        activity.setActivityName("测试活动");
+        activity.setActivityDesc("仅用于插入数据测试");
+        activity.setBeginDateTime(new Date());
+        activity.setEndDateTime(new Date());
+        activity.setStockCount(100);
+        activity.setTakeCount(10);
+        activity.setState(0);
+        activity.setCreator("xiaofuge");
+        activityDao.insert(activity);
+    }
+
+    @Test
+    public void test_select() {
+        Activity activity = activityDao.queryActivityById(100001L);
+        logger.info("测试结果：{}", JSON.toJSONString(activity));
+    }
+
+}
